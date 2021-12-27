574671640edf75cb59f0743a893b20057b8a1713
Oleg Kozyrev <okozyrev@ozon.ru>
2021-12-27 16:09:25 +0300

    add generation auth link

---
+++ internal/app/service/items_keeper/generate_authorization_link.go
@@ -0,0 +1,21 @@
+package items_keeper
+
+import (
+	"context"
+
+	"github.com/olezhek28/items-keeper/internal/app/repository"
+)
+
+func (s *ItemsKeeperService) GenerateAuthorizationLink(ctx context.Context, chatID int64) (string, error) {
+	requestToken, err := s.pocketClient.GetRequestToken(ctx, chatID)
+	if err != nil {
+		return "", err
+	}
+
+	err = s.tokensRepository.Save(chatID, requestToken, repository.BucketRequestTokens)
+	if err != nil {
+		return "", err
+	}
+
+	return s.pocketClient.GetAuthorizationLink(requestToken)
+}
