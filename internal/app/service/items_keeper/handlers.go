5a22dda32236eb694718bdac4e919db94bdafd08
Oleg Kozyrev <okozyrev@ozon.ru>
2021-12-28 01:09:11 +0300

    done handleMessage

--- internal/app/service/items_keeper/handlers.go
+++ internal/app/service/items_keeper/handlers.go
@@ -3,9 +3,10 @@ package items_keeper
 import (
 	"context"
 	"fmt"
-	"log"
+	"net/url"
 
 	tgBotAPI "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/olezhek28/items-keeper/internal/app/repository"
 )
 
 const (
@@ -13,7 +14,12 @@ const (
 )
 
 const (
-	replyStartTemplate = "Привет! Чтобы сохранять ссылки в своем Pocket аккаунте, для начала тебе необходимо дать мне на это доступ. Для этого переходи по ссылке:\n%s"
+	replyStartTemplate     = "Привет! Чтобы сохранять ссылки в своем Pocket аккаунте, для начала тебе необходимо дать мне на это доступ. Для этого переходи по ссылке:\n%s"
+	replayAlreadyAuthorize = "Ты уже авторизирован, брат! можешь пользоваться. Кидай ссылку, а я её сохраню."
+	replaySuccessSaveLink  = "Твоя ссылка успешно сохранена!"
+	replayFailedSaveLink   = "Не удалось сохранить ссылку:( Попробуй ещё разок)"
+	replyInvalidLink       = "Твоя ссылка невалидная:( Попробуй исправить это, я верю в тебя!"
+	replayUnauthorized     = "Ты ещё не авторизирован. Вызови команду /start"
 )
 
 func (s *ItemsKeeperService) handleCommand(message *tgBotAPI.Message) error {
@@ -26,14 +32,67 @@ func (s *ItemsKeeperService) handleCommand(message *tgBotAPI.Message) error {
 }
 
 func (s *ItemsKeeperService) handleMessage(message *tgBotAPI.Message) error {
-	log.Printf("[%s] %s", message.From.UserName, message.Text)
+	msg := tgBotAPI.NewMessage(message.Chat.ID, replaySuccessSaveLink)
 
-	msg := tgBotAPI.NewMessage(message.Chat.ID, message.Text)
+	_, err := url.ParseRequestURI(message.Text)
+	if err != nil {
+		msg = tgBotAPI.NewMessage(message.Chat.ID, replyInvalidLink)
+		return s.tgClient.Send(msg)
+	}
+
+	accessToken, err := s.getAccessToken(message.Chat.ID)
+	if err != nil {
+		msg = tgBotAPI.NewMessage(message.Chat.ID, replayUnauthorized)
+		return s.tgClient.Send(msg)
+	}
+
+	err = s.pocketClient.Add(context.Background(), accessToken, message.Text)
+	if err != nil {
+		msg = tgBotAPI.NewMessage(message.Chat.ID, replayFailedSaveLink)
+		return s.tgClient.Send(msg)
+	}
 
 	return s.tgClient.Send(msg)
 }
 
 func (s *ItemsKeeperService) handleCommandStart(message *tgBotAPI.Message) error {
+	var msg tgBotAPI.MessageConfig
+
+	_, err := s.getAccessToken(message.Chat.ID)
+	if err != nil {
+		return s.initAuthorizationProcess(message)
+	}
+
+	msg = tgBotAPI.NewMessage(message.Chat.ID, replayAlreadyAuthorize)
+
+	return s.tgClient.Send(msg)
+}
+
+func (s *ItemsKeeperService) handleUnknownCommand(message *tgBotAPI.Message) error {
+	msg := tgBotAPI.NewMessage(message.Chat.ID, "Я не знаю такую команду")
+
+	return s.tgClient.Send(msg)
+}
+
+func (s *ItemsKeeperService) generateAuthorizationLink(ctx context.Context, chatID int64) (string, error) {
+	requestToken, err := s.pocketClient.GetRequestToken(ctx, chatID)
+	if err != nil {
+		return "", err
+	}
+
+	err = s.tokensRepository.Save(chatID, requestToken, repository.BucketRequestTokens)
+	if err != nil {
+		return "", err
+	}
+
+	return s.pocketClient.GetAuthorizationLink(requestToken)
+}
+
+func (s *ItemsKeeperService) getAccessToken(chatID int64) (string, error) {
+	return s.tokensRepository.Get(chatID, repository.BucketAccessTokens)
+}
+
+func (s *ItemsKeeperService) initAuthorizationProcess(message *tgBotAPI.Message) error {
 	ctx := context.Background()
 	authLink, err := s.generateAuthorizationLink(ctx, message.Chat.ID)
 	if err != nil {
@@ -41,12 +100,6 @@ func (s *ItemsKeeperService) handleCommandStart(message *tgBotAPI.Message) error
 	}
 
 	msg := tgBotAPI.NewMessage(message.Chat.ID, fmt.Sprintf(replyStartTemplate, authLink))
-
-	return s.tgClient.Send(msg)
-}
-
-func (s *ItemsKeeperService) handleUnknownCommand(message *tgBotAPI.Message) error {
-	msg := tgBotAPI.NewMessage(message.Chat.ID, "Я не знаю такую команду")
 
 	return s.tgClient.Send(msg)
 }
